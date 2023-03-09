232cc24341b9d9efc7d6ca007f484fb54818ca02
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2023-03-09 12:41:20 +0100

    Update cw3PS.c

--- cw3PS.c
+++ cw3PS.c
@@ -11,6 +11,22 @@ int main()
   
 //2.2.4
   
+    int n;
+    int wynik = 2;
+    scanf("%d", &n);
+
+    if (n<0)
+    {
+        n=n*(-1);
+    }
+
+    for(int i=1;i<n;i++)
+    {
+        wynik=wynik*2;
+    }
+
+    printf("%d", wynik);
+
 //2.2.8
   
 //2.2.12
