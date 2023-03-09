953a24f7c165765551fe58fd227776d639c8e599
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2023-03-09 11:49:41 +0100

    Create cw3PS.c

---
+++ cw3PS.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+  
+//2.2.2
+  
+//2.2.4
+  
+//2.2.8
+  
+//2.2.12
+  
+//2.2.17
+  
+//2.2.20
+
+//2.2.24
+  
+//2.2.29
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+    return 0;
+}
