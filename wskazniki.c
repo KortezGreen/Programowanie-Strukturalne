b6356419943db8bd3bbe79ee6f08613f0b9289d2
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2023-03-23 13:04:44 +0100

    Update wskazniki.c

--- wskazniki.c
+++ wskazniki.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#3.2.1
-int najm(int *a, int *b)
-{
-    if(*a>*b)
-    {
-        return 1;
-    }
-    else if(*a<*b)
-    {
-        return 0;
-    }
-    else
-    {
-        return -1;
-    }
-}
+//3.2.1
+//int najm(int *a, int *b)
+//{
+//    if(*a>*b)
+//    {
+//        return 1;
+//    }
+//    else if(*a<*b)
+//    {
+//        return 0;
+//    }
+//    else
+//    {
+//        return -1;
+//    }
+//}
 
+//3.2.2
+//int najws(int *a, int *b)
+//{
+//    if(*a>*b)
+//    {
+//        printf("%i", *b);
+//    }
+//    else if(*a<*b)
+//    {
+//        printf("%i", *a);
+//    }
+//    else
+//    {
+//        printf("%i", *a);
+//    }
+//}
 
-
+//3.2.4
 
 
 
@@ -30,26 +46,32 @@ int najm(int *a, int *b)
 int main()
 {
 
-#3.2.1
-int a;
-int b;
-scanf("%i %i", &a, &b);
-int wynik = najm(&a, &b);
+//3.2.1
+//int a;
+//int b;
+//scanf("%i %i", &a, &b);
+//int wynik = najm(&a, &b);
+//
+//    if(wynik == 1)
+//    {
+//        printf("%i", b);
+//    }
+//    else if(wynik == 0)
+//    {
+//        printf("%i", a);
+//    }
+//    else
+//    {
+//        printf("Liczby sa rowne");
+//    }
 
-    if(wynik == 1)
-    {
-        printf("%i", b);
-    }
-    else if(wynik == 0)
-    {
-        printf("%i", a);
-    }
-    else
-    {
-        printf("Liczby sa rwone");
-    }
+//3.2.2
+//int a;
+//int b;
+//scanf("%i %i", &a, &b);
+//najws(&a, &b);
 
-#3.
+//3.2.4
 
 
 return 0;
