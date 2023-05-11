ae10f4801b0bb27314f5eda25ae36572185a28bd
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2023-05-11 13:04:04 +0200

    Create struktury.c

---
+++ struktury.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//7.2.1
+//struct trojkat {
+//double a;
+//double b;
+//double c;};
+//
+//double polet (struct trojkat tro)
+//{
+//    double pole = tro.a+tro.b+tro.c;
+//    return pole;
+//}
+
+int main()
+{
+
+//7.2.1
+//struct trojkat tro = {2, 3, 5};
+//double wynik = polet(trojkat);
+//printf("%lf\n",wynik);
+
+//7.2.9
+//struct lista{
+//int ll;
+//int A;
+//int B;};
+//
+//int* A;
+//int* B;
+
+//7.2.10
+//union super_int{
+//int G;
+//unsigned int H;};
+
+//7.2.
+
+
+    return 0;
+}
